3ab8dd07ad0b3346fea2e5cf59c1b510bf7cd940
William Cao <williamacao@gmail.com>
2021-02-06 13:25:41 -0800

    commit tree 6101e86ab1ce57b70e5ca5d2e614e4533e56d73d

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,24 @@
+class Appointment
+    attr_accessor :date, :patient, :doctor
 
+    @@all = []
+    
+    def initialize(date, patient, doctor)
+        @patient = patient
+        @doctor = doctor
+        @date = date
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def patient
+        @patient
+    end
+
+    def doctor
+        @doctor
+    end
+end
